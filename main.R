3190bab4012074d75e7937b1cd8c4eef3bcce3d3
schauminn <schauminn@users.noreply.github.com>
2015-07-31 21:48:55 +0800

    Added whitespace cleaning function

--- main.R
+++ main.R
@@ -4,3 +4,48 @@
 #2. Phone numbers
 #3. ZIP codes
 #4. Names
+
+DirtyStreetNames <- read.csv("Dirty Street Names.csv", stringsAsFactors = FALSE)
+DirtyStreetNames <- as.vector(unlist(DirtyStreetNames))
+
+#This will clear up excess whitespace with 4 options----
+#1. Leading spaces
+#2. Trailing spaces
+#3. Spaces in between
+#4. All of the above
+
+CleanSpaces <- function(data, option=c("leading", "trailing", "between", "all"), spacing = 1){
+                        
+  
+  SPACE <- paste(rep(" ",spacing), collapse = "")
+  
+  Leading <- function(data){
+    data <- gsub("^\\s+", "", data)
+    return(data)
+  }
+  
+  Trailing <- function(data){
+    data <- gsub("\\s+$", "", data)
+    return(data)
+  }
+  
+  Between <- function(data){
+    data <- gsub("\\s+", SPACE, data)
+    return(data)
+  }
+  
+  All <- function(data){
+    data <- gsub("^\\s+|\\s+$", "", data)
+    data <- gsub("\\s+", SPACE, data)
+    return(data)
+  }
+  
+  x <- data
+  
+  OUT <- switch(option,
+                leading = Leading(x),
+                trailing = Trailing(x),
+                between = Between(x),
+                all = All(x))
+  return(OUT)
+}
